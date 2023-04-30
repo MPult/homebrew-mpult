08497083f9b584997d0dbdba039ddc7e104dee68
Electron <one.electron109@protonmail.com>
2023-04-30 19:11:39 +0200

    added rexit

---
+++ Formula/rexit.rb
@@ -0,0 +1,17 @@
+class Itex < Formula
+  desc "Liberate your Reddit Chats"
+  homepage ""
+  url "https://github.com/mpult/rexit/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 ""
+  license "GPL-3.0-or-later"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", "--no-default-features", "--path", ".", "--root", prefix.to_s
+  end
+
+  test do
+    system "false"
+  end
+endk
